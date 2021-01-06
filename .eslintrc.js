512d343c97d08c59c273df55388c809b6d2a7cf1
Goga Papidze <goga.papidze@lb.com>
2021-01-06 23:19:38 +0400

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2021': true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-hooks'],
+  rules: {
+    'react/prop-types': 0,
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+  },
+}
