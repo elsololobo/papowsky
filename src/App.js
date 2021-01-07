35ef1ccd013ed69f962b8e973c7e63170e933e20
Goga Papidze <goga.papidze@lb.com>
2021-01-07 15:37:21 +0400

    added header code and styling

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import Home from './pages/home/home'
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 import Shop from './pages/shop/shop'
+import Header from './components/header/header'
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Switch>
         <Route exact={true} path={'/'} component={Home} />
         <Route exact={true} path={'/shop'} component={Shop} />
