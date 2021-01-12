0ad677376788675b888f77959124bed3bd914d31
Goga Papidze <goga.papidze@lb.com>
2021-01-12 23:23:26 +0400

    updating the app to redirect to home when ser is signed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import './App.css'
 import Home from './pages/home/home'
 import React, { useEffect } from 'react'
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch, Redirect } from 'react-router-dom'
 import Shop from './pages/shop/shop'
 import Header from './components/header/header'
 import Login from './pages/login/login'
 import { auth, createUserProfileDocument } from './firebase/firebase.utils'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { setCurrentUser } from './redux/slices/userSlice'
 
 function App() {
   const dispatch = useDispatch()
+  const currentUser = useSelector((state) => state.user.currentUser)
+
   useEffect(() => {
     let unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
       if (userAuth) {
@@ -29,7 +31,11 @@ function App() {
       <Switch>
         <Route exact={true} path={'/'} component={Home} />
         <Route exact={true} path={'/shop'} component={Shop} />
-        <Route exact={true} path={'/sign-in'} component={Login} />
+        <Route
+          exact={true}
+          path={'/sign-in'}
+          render={() => (currentUser ? <Redirect to={'/'} /> : <Login />)}
+        />
       </Switch>
     </div>
   )
